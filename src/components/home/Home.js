8ec4e56753765958d452fab223c55759d7125948
javieramirez90 <javier.ramirez@aiesec.net>
2018-12-13 12:02:31 -0600

    Testing routes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const Home = () => (
   <div>
     <h1>Hola Mike, ya ca'mate po'favo</h1>
-    <Link to='/'>
+    <Link to='/api/authors'>
       <button>Show me the Mortys</button>
     </Link>
-    <Link>
+    {/* <Link>
       <button>Agrega un Morty</button>
-    </Link>
+    </Link> */}
   </div>
   
   )

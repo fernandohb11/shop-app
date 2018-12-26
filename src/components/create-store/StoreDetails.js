302d34c9f19017c2ab4eaedd1ea497b73da15a31
Fernando Hernandez <fernandohernandez@MacBook-Pro-de-Fernando.local>
2018-12-25 18:31:52 -0600

    create shop works

--- src/components/create-store/StoreDetails.js
+++ src/components/create-store/StoreDetails.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react'
+import { Input, Upload, Icon, message } from 'antd';
+
+
+
+
 
 export class StoreDetails extends Component {
+  continue = e =>{
+    e.preventDefault();
+    this.props.nextStep();
+  }
+
+
+
+  
   render() {
+    const { values } = this.props
     return (
       <div>
-        <h1>Hola desde tienda de detalles</h1>
-      </div>
+        <React.Fragment>
+        
+       <form>
+        <Input id='titulo' placeholder='Ingresa el Titulo' defaultValue={values.titulo} onChange={this.props.handleChange('titulo') } />
+        <br/>
+        <Input id='descripcion' placeholder='Ingresa la Descripción' defaultValue={values.descripcion} onChange={this.props.handleChange('descripcion') } />
+        <br/>
+        <Input id='precio' placeholder='Ingresa el Precio' defaultValue={values.precio} onChange={this.props.handleChange('precio') } />
+        <br/>
+        <Input id='titulo' placeholder='Ingresa las Categorías' defaultValue={values.categorias} onChange={this.props.handleChange('categorias') } />
+        <br/>
+       <button onClick={this.continue}>Continuar</button>
+       </form>
+       
+
+        </React.Fragment>
+    </div>
+
+    
+
     )
+    
   }
 }
 
